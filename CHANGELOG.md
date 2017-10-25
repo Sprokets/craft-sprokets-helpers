112854490edf49296f72006238eba7f9b75e2085
Dan Ott <dan@dtott.com>
2017-10-25 10:58:05 -0400

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Sprokets Ralston Helpers Changelog
+
+## 1.0.0 -- 2017.10.25
+
+* Initial release
+
+Brought to you by [Sprokets](http://sprokets.net)
---
+++ sproketshelpers/translations/en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Sprokets Ralston Helpers plugin for Craft CMS
+ *
+ * Sprokets Ralston Helpers Translation
+ *
+ * @author    Sprokets
+ * @copyright Copyright (c) 2017 Sprokets
+ * @link      http://sprokets.net
+ * @package   SproketsRalstonHelpers
+ * @since     1.0.0
+ */
+
+return array(
+    'Translate me' => 'To this',
+);
