075df18056d109e01356269da64b0a76a1340d7c
Dan Ott <dan@dtott.com>
2020-08-05 09:42:06 -0400

    add get_type, of_type

--- src/twigextensions/SproketshelpersTwigExtension.php
+++ src/twigextensions/SproketshelpersTwigExtension.php
@@ -54,9 +54,70 @@ class SproketshelpersTwigExtension extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('emailObfuscate', [$this, 'emailObfuscate']),
+          new \Twig\TwigFilter('emailObfuscate', [$this, 'emailObfuscate']),
+          new \Twig\TwigFilter('get_type', [$this, 'twig_get_type'] )
         ];
     }
+
+    public function getTests()
+    {
+      return [
+        new \Twig\TwigTest('of_type', [$this, 'twig_of_type'])
+      ];
+    }
+
+
+    public function twig_of_type($var, $typeTest=null, $className=null)
+	{
+
+		switch ($typeTest)
+		{
+			default:
+				return false;
+				break;
+
+			case 'array':
+				return is_array($var);
+				break;
+
+			case 'bool':
+				return is_bool($var);
+				break;
+
+			case 'class':
+				return is_object($var) === true && get_class($var) === $className;
+				break;
+
+			case 'float':
+				return is_float($var);
+				break;
+
+			case 'int':
+				return is_int($var);
+				break;
+
+			case 'numeric':
+				return is_numeric($var);
+				break;
+
+			case 'object':
+				return is_object($var);
+				break;
+
+			case 'scalar':
+				return is_scalar($var);
+				break;
+
+			case 'string':
+				return is_string($var);
+				break;
+		}
+	}
+
+	public function twig_get_type($var)
+	{
+		return gettype( $var );
+	}
 
 
   protected function pregEmails($string)
