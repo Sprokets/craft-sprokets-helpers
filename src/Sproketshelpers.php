5a1f2c6dfaf992bacfda54dd22dc5a4903793392
sfriel <G0g3t1t>
2023-02-27 13:39:46 -0500

    wrap logic in onInit callback

--- src/Sproketshelpers.php
+++ src/Sproketshelpers.php
@@ -81,75 +81,76 @@ class Sproketshelpers extends Plugin
     {
         parent::init();
         self::$plugin = $this;
-
         self::$nonce  = Craft::$app->getConfig()->general->isContentBaseRequest ? false : base64_encode(random_bytes(32));
-
-        if (!\Craft::$app->request->getIsConsoleRequest()) {
-            // if (\Craft::$app->request->getIsCpRequest() && !\Craft::$app->user->isGuest) {
-            //     $this->view->registerAssetBundle("sprokets\\sproketshelpers\\assetbundles\\discloseassets\\DiscloseAssetsBundle");
-            // }
-            if (
-                \Craft::$app->request->getIsCpRequest() &&
-                \Craft::$app->user &&
-                \Craft::$app->user->identity &&
-                \Craft::$app->user->identity->admin &&
-                Craft::$app->getConfig()->general->allowAdminChanges
-            ) {
-                // $this->view->registerAssetBundle(Sproketshelpers_SidebarEnhanceAsset::class);
-                $this->view->registerAssetBundle("sprokets\\sproketshelpers\\assetbundles\\sidebarenhance\\SidebarEnhanceAsset");
-            }
-        }
-
-        Craft::$app->view->registerTwigExtension(new SproketshelpersTwigExtension());
-
-        // Register our variables
-        Event::on(
-            CraftVariable::class,
-            CraftVariable::EVENT_INIT,
-            function (Event $event) {
-                /** @var CraftVariable $variable */
-                $variable = $event->sender;
-                $variable->set('sproketshelpers', SproketshelpersVariable::class);
-            }
-        );
-
-        // Do something after we're installed
-        Event::on(
-            Plugins::class,
-            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
-            function (PluginEvent $event) {
-                if ($event->plugin === $this) {
-                    // We were just installed
+        // any code that depends on other plugins to be loaded should be wrapped in onInit function
+        Craft::$app->onInit(function () {
+            if (!\Craft::$app->request->getIsConsoleRequest()) {
+                // if (\Craft::$app->request->getIsCpRequest() && !\Craft::$app->user->isGuest) {
+                //     $this->view->registerAssetBundle("sprokets\\sproketshelpers\\assetbundles\\discloseassets\\DiscloseAssetsBundle");
+                // }
+                if (
+                    \Craft::$app->request->getIsCpRequest() &&
+                    \Craft::$app->user &&
+                    \Craft::$app->user->identity &&
+                    \Craft::$app->user->identity->admin &&
+                    Craft::$app->getConfig()->general->allowAdminChanges
+                ) {
+                    // $this->view->registerAssetBundle(Sproketshelpers_SidebarEnhanceAsset::class);
+                    $this->view->registerAssetBundle("sprokets\\sproketshelpers\\assetbundles\\sidebarenhance\\SidebarEnhanceAsset");
                 }
             }
-        );
 
-        /**
-         * Logging in Craft involves using one of the following methods:
-         *
-         * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
-         * Craft::info(): record a message that conveys some useful information.
-         * Craft::warning(): record a warning message that indicates something unexpected has happened.
-         * Craft::error(): record a fatal error that should be investigated as soon as possible.
-         *
-         * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
-         *
-         * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
-         * the category to the method (prefixed with the fully qualified class name) where the constant appears.
-         *
-         * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
-         * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
-         *
-         * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
-         */
-        Craft::info(
-            Craft::t(
-                'sprokets-helpers',
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
-        );
+            Craft::$app->view->registerTwigExtension(new SproketshelpersTwigExtension());
+
+            // Register our variables
+            Event::on(
+                CraftVariable::class,
+                CraftVariable::EVENT_INIT,
+                function (Event $event) {
+                    /** @var CraftVariable $variable */
+                    $variable = $event->sender;
+                    $variable->set('sproketshelpers', SproketshelpersVariable::class);
+                }
+            );
+
+            // Do something after we're installed
+            Event::on(
+                Plugins::class,
+                Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+                function (PluginEvent $event) {
+                    if ($event->plugin === $this) {
+                        // We were just installed
+                    }
+                }
+            );
+
+            /**
+             * Logging in Craft involves using one of the following methods:
+             *
+             * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
+             * Craft::info(): record a message that conveys some useful information.
+             * Craft::warning(): record a warning message that indicates something unexpected has happened.
+             * Craft::error(): record a fatal error that should be investigated as soon as possible.
+             *
+             * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
+             *
+             * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
+             * the category to the method (prefixed with the fully qualified class name) where the constant appears.
+             *
+             * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
+             * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
+             *
+             * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
+             */
+            Craft::info(
+                Craft::t(
+                    'sprokets-helpers',
+                    '{name} plugin loaded',
+                    ['name' => $this->name]
+                ),
+                __METHOD__
+            );
+        });
     }
 
     // Protected Methods
